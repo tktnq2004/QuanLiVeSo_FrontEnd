8b7a13096d9fdb87efcbe13c16be77cb108c1727
tktnq2004 <tktnq2004@gmail.com>
2026-06-09 02:59:53 +0700

    fix URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-// const api = axios.create({
-//     baseURL: 'https://quanliveso-webserver.onrender.com/api'
-// });
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://quanliveso-webserver.onrender.com/api'
 });
+// const api = axios.create({
+//     baseURL: 'http://localhost:5000/api'
+// });
 
 export default api;
